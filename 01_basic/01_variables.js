e231bb8ea35b369eab14e86c8871f94fb6a4c608
Jaywardhan Sharma <133585991+shjay06@users.noreply.github.com>
2025-07-25 14:09:28 +0000

    Variables done

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 1
+let accountEmail = "jaywardhann@gmail.com"
+var accountPassword = "1234"
+accountCity = "Jaipur"
+
+// accountId = 2 //not allowed
+console.log(accountId);
+
+accountEmail = "jah@gmail.com"
+accountPassword = "gh"
+accountCity = "Bangaluru"
+console.table([accountId, accountEmail, accountPassword,accountCity])
+ /*
+ Prefer not use var to abvoid function scope
+  */
