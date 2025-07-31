090704a677a8ced1f4ef2dccac48375473c401d0
Jaywardhan Sharma <133585991+shjay06@users.noreply.github.com>
2025-07-31 15:42:09 +0000

    Maths and Number

---
+++ 01_basic/06_numbs_and_maths.js
@@ -0,0 +1,28 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(400)
+// console.log(balance);
+ 
+// console.log(balance.toString());//toString property leads to use methods of string as number have very less functions
+
+const otherNumber = 123.45
+// console.log(otherNumber.toPrecision(4));
+ 
+//---------------------MATHS------------------------------
+
+console.log(Math);
+console.log(Math.abs(-2));
+
+console.log(Math.round(4.7));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.9));
+
+console.log(Math.max(4, 3, 5, 6));
+console.log(Math.min(4, 3, 5, 6));
+
+console.log(Math.random()); //this sfunctions will give value between 0 to 1
+console.log((Math.random()*10)+1);
+
+
+
