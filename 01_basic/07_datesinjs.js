0587b60f3a08e1750254d461b1b6288702f06e74
Jaywardhan Sharma <133585991+shjay06@users.noreply.github.com>
2025-08-06 15:45:13 +0000

    Date completed

---
+++ 01_basic/07_datesinjs.js
@@ -0,0 +1,14 @@
+//Date
+
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2023, 0, 23)
+// console.log(myCreatedDate.toDateString());
+let myCreatedDate = new Date("01-14-2023")
+console.log(myCreatedDate.toDateString());
+
+let mytimeStamp = Date.now()
