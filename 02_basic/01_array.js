3c4ce93cec21b894314b190d4820253d46ec4d83
Jaywardhan Sharma <133585991+shjay06@users.noreply.github.com>
2025-09-01 13:19:58 +0000

    Array part1

---
+++ 02_basic/01_array.js
@@ -0,0 +1,25 @@
+//array
+
+const myArr = [0,1,2,5]
+const myHeros = ["shaktiman","naagraj"]
+
+const myArr2 = new Array(1,23,4,5)
+
+console.log(myArr[0]);
+
+//Array methods
+// myArr.push(7)
+// myArr.push(8)
+// myArr.pop()
+
+// myArr.unshift(70) // adds on the 0th position
+// myArr.shift()
+
+// console.log(myArr.includes(9))
+// console.log(myArr.indexOf(2))
+
+// const newArr = myArr.join() //joins and converts into a string
+
+// console.log(myArr);
+// console.log(newArr)
+
